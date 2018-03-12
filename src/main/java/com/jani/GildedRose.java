c399a6e2d34bc3397da2a08ba2368768bb759ca2
Jani Peng <1184360257@qq.com>
2018-03-12 21:39:46 +0800

    remove unused code

--- src/main/java/com/jani/GildedRose.java
+++ src/main/java/com/jani/GildedRose.java
@@ -2,34 +2,15 @@ package com.jani;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
 
 
-public class GildedRose {
+class GildedRose {
 
     static List<Item> items = null;
 
-    /**
-     * @param args
-     */
-    public static void main(String[] args) {
-
-        System.out.println("OMGHAI!");
-
-        items = new ArrayList<Item>();
-        items.add(new Item("+5 Dexterity Vest", 10, 20));
-        items.add(new Item("Aged Brie", 2, 0));
-        items.add(new Item("Elixir of the Mongoose", 5, 7));
-        items.add(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
-        items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
-        items.add(new Item("Conjured Mana Cake", 3, 6));
-
-        updateQuality();
-    }
-
-    public static void updateQuality() {
-        for (int i = 0; i < items.size(); i++) {
-            items.get(i).updateQuality();
-        }
+    static void updateQuality() {
+        items.forEach(item -> item.updateQuality());
     }
 
 }
