39aaeae67d6e34a44acb42c8e1edaacc87ccc21f
Jani Peng <1184360257@qq.com>
2018-03-12 22:09:53 +0800

    remove code smells by Ideai and create stategy factory

--- src/main/java/com/jani/Item.java
+++ src/main/java/com/jani/Item.java
@@ -1,58 +1,41 @@
 package com.jani;
 
-public class Item {
-    public String name;
-    public int sellIn;
-    public int quality;
+import static com.jani.ItemStrategyFactory.createItemStrategy;
 
+class Item {
+    private String name;
+    private int sellIn;
+    private int quality;
     private ItemStrategy itemStrategy;
 
-    public Item(String name, int sellIn, int quality) {
-        this.setName(name);
-        this.setSellIn(sellIn);
-        this.setQuality(quality);
+    Item(String name, int sellIn, int quality) {
+        this.name = name;
+        this.sellIn = sellIn;
+        this.quality = quality;
+        createItemStrategyByName(name);
+    }
+
+    private void createItemStrategyByName(String name) {
+        itemStrategy = createItemStrategy(name);
     }
 
     void updateQuality() {
-        switch (name) {
-            case "Aged Brie":
-                itemStrategy = new AgedBrie();
-                break;
-            case "Backstage passes to a TAFKAL80ETC concert":
-                itemStrategy = new BackstagePasses();
-                break;
-            case "Sulfuras, Hand of Ragnaros":
-                itemStrategy = new Sulfuras();
-                break;
-            default:
-                itemStrategy = new NormalItem();
-                break;
-        }
         itemStrategy.update(this);
     }
 
-    /* Generated getter and setter code */
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getSellIn() {
+    int getSellIn() {
         return sellIn;
     }
 
-    public void setSellIn(int sellIn) {
+    void setSellIn(int sellIn) {
         this.sellIn = sellIn;
     }
 
-    public int getQuality() {
+    int getQuality() {
         return quality;
     }
 
-    public void setQuality(int quality) {
+    void setQuality(int quality) {
         this.quality = quality;
     }
 }
---
+++ src/main/java/com/jani/ItemStrategyFactory.java
@@ -0,0 +1,17 @@
+package com.jani;
+
+class ItemStrategyFactory {
+
+    static ItemStrategy createItemStrategy(String name) {
+        switch (name) {
+            case "Aged Brie":
+                return new AgedBrie();
+            case "Backstage passes to a TAFKAL80ETC concert":
+                return new BackstagePasses();
+            case "Sulfuras, Hand of Ragnaros":
+                return  new Sulfuras();
+            default:
+                return new NormalItem();
+        }
+    }
+}
