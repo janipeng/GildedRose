6089233efa405c1ea9b8cfa40c59ecf86e98a320
Jani Peng <1184360257@qq.com>
2018-03-13 21:15:41 +0800

    refactor for Item

--- src/main/java/com/jani/Item.java
+++ src/main/java/com/jani/Item.java
@@ -1,33 +1,33 @@
 package com.jani;
 
-public class Item {
-    public String name;
-	public int sellIn; 
-    public int quality; 
-    
-    public Item(String name, int sellIn, int quality) {
-		this.setName(name);
-		this.setSellIn(sellIn);
-		this.setQuality(quality);
-	}
-    
-	/* Generated getter and setter code */
-    public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public int getSellIn() {
-		return sellIn;
-	}
-	public void setSellIn(int sellIn) {
-		this.sellIn = sellIn;
-	}
-	public int getQuality() {
-		return quality;
-	}
-	public void setQuality(int quality) {
-		this.quality = quality;
-	}
+class Item {
+    private String name;
+    private int sellIn;
+    private int quality;
+
+    Item(String name, int sellIn, int quality) {
+        this.name = name;
+        this.sellIn = sellIn;
+        this.quality = quality;
+    }
+
+    String getName() {
+        return name;
+    }
+
+    int getSellIn() {
+        return sellIn;
+    }
+
+    void setSellIn(int sellIn) {
+        this.sellIn = sellIn;
+    }
+
+    int getQuality() {
+        return quality;
+    }
+
+    void setQuality(int quality) {
+        this.quality = quality;
+    }
 }
