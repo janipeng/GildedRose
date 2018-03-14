6e4b40a1b523de17660563394bd4b8b1edeabfb6
Jani Peng <1184360257@qq.com>
2018-03-14 23:30:56 +0800

    gilded rose refactoring

--- src/main/java/com/jani/GildedRose.java
+++ src/main/java/com/jani/GildedRose.java
@@ -2,26 +2,15 @@ package com.jani;
 
 import java.util.List;
 
+import static com.jani.ItemStrategyFactory.createItemStrategy;
+
 
 class GildedRose {
 
     static List<Item> items = null;
 
     static void updateQuality() {
-        for (Item item : items) {
-            ItemStrategy itemStrategy;
-            switch (item.getName()) {
-                case "Aged Brie":
-                    itemStrategy = new AgedBrie();
-                case "Backstage passes to a TAFKAL80ETC concert":
-                    itemStrategy = new BackstagePasses();
-                case "Sulfuras, Hand of Ragnaros":
-                    itemStrategy = new Sulfuras();
-                default:
-                    itemStrategy = new NormalItem();
-            }
-            itemStrategy.update(item);
-        }
+        items.forEach(item -> createItemStrategy(item.getName()).update(item));
     }
 
 }
--- src/main/java/com/jani/Item.java
+++ src/main/java/com/jani/Item.java
@@ -1,33 +1,29 @@
 package com.jani;
 
-public class Item {
-    public String name;
-	public int sellIn; 
-    public int quality; 
+class Item {
+    private String name;
+	private int sellIn;
+    private int quality;
     
-    public Item(String name, int sellIn, int quality) {
-		this.setName(name);
-		this.setSellIn(sellIn);
-		this.setQuality(quality);
+    Item(String name, int sellIn, int quality) {
+		this.name = name;
+		this.sellIn = sellIn;
+		this.quality = quality;
 	}
     
-	/* Generated getter and setter code */
-    public String getName() {
+	String getName() {
 		return name;
 	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public int getSellIn() {
+	int getSellIn() {
 		return sellIn;
 	}
-	public void setSellIn(int sellIn) {
+	void setSellIn(int sellIn) {
 		this.sellIn = sellIn;
 	}
-	public int getQuality() {
+	int getQuality() {
 		return quality;
 	}
-	public void setQuality(int quality) {
+	void setQuality(int quality) {
 		this.quality = quality;
 	}
 }
